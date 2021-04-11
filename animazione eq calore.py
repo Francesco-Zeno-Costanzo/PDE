a4e188938893a7342d5d43877af8730f4b20ed89
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2021-04-11 20:40:18 +0200

    Update animazione eq calore.py

--- animazione eq calore.py
+++ animazione eq calore.py
@@ -1,22 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
 N=100
 x=np.linspace(0, N, N)
-tstep=10000
+tstep=5000
 T=np.zeros((N,tstep))
 #Temperatura iniziale
 T[0:N,0]=500*np.exp(-((50-x)/20)**2)
 
-ts=1
-tau=1*ts
+tau=10
+D=0.5*tau
+dx=0.01
+dt=1e-5 #dt<=dx**2/D
+r = D*dt/dx**2
 
 
 for time in range(1,tstep):
     for i in range(0,N-1):
-        T[i,time]=T[i,time-1]+0.5*tau/ts*(T[i-1,time-1]+T[i+1,time-1]-2*T[i,time-1])
-T[0,time]=T[1,time]
-T[N-1,time]=T[N-2,time]
+        T[i,time]=T[i,time-1] + r*(T[i-1,time-1]+T[i+1,time-1]-2*T[i,time-1])
+#T[0,time]=T[1,time] #per avere bordi non fissi
+#T[N-1,time]=T[N-2,time]
 
 
 fig = plt.figure()
@@ -29,7 +33,7 @@ def animate(i):
     return line,
 
 
-anim = animation.FuncAnimation(fig, animate, frames=tstep,interval=10, blit=True, repeat=True)
+anim = animation.FuncAnimation(fig, animate, frames=tstep, interval=10, blit=True, repeat=True)
 
 plt.grid()
 plt.title('Diffusione del calore')
