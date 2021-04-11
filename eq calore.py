1cbd7696d53a352863e74156452dda1b2863a292
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2021-04-11 20:39:28 +0200

    Update eq calore.py

--- eq calore.py
+++ eq calore.py
@@ -10,20 +10,22 @@ T=np.zeros((N,tstep))
 #Temperatura iniziale
 T[0:N,0]=500*np.exp(-((50-x)/20)**2)
 
-ts=0.1
-tau=0.9*ts
-
+tau=10
+D=0.5*tau
+dx=0.01
+dt=1e-5 #dt<=dx**2/D
+r = D*dt/dx**2
 
 for time in range(1,tstep):
     for i in range(1,N-1):
-        T[i,time]=T[i,time-1]+0.5*tau/ts*(T[i-1,time-1]+T[i+1,time-1]-2*T[i,time-1])
-    T[0,time]=T[1,time]
-    T[N-1,time]=T[N-2,time]
+        T[i,time]=T[i,time-1] + r*(T[i-1,time-1]+T[i+1,time-1]-2*T[i,time-1])
+#T[0,time]=T[1,time] #per avere bordi non fissi
+#T[N-1,time]=T[N-2,time]
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-gridx , gridy = np.meshgrid(range(tstep),range(N))
-ax.plot_surface(gridx,gridy,T,cmap=mp.cm.coolwarm,vmax=250,linewidth=0,rstride=2, cstride=100)
+gridx , gridy = np.meshgrid(range(tstep), range(N))
+ax.plot_surface(gridx,gridy,T, cmap=mp.cm.coolwarm,vmax=250,linewidth=0,rstride=2, cstride=100)
 ax.set_title('Diffusione del calore')
 ax.set_xlabel('Tempo')
 ax.set_ylabel('Lunghezza')
