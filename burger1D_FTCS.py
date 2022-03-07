af2922e4ec45e1afd8d5216bef526bc8e649f419
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2022-03-07 17:18:00 +0100

    Create burger1D_FTCS.py

---
+++ burger1D_FTCS.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+N = 201
+T = 800
+dx =  2*np.pi/(N-1)
+nu = 0.07
+dt = 0.002
+
+x  = np.linspace(0, 2*np.pi, N)
+
+sol = np.zeros((N, T+1))
+
+
+#condizione iniziale
+sol[:,0] = np.sin(x)
+
+for n in range(T):
+    for i in range(1, N - 1):
+        #eulero per le derivate seconde
+        ddsol = sol[i+1, n] - 2*sol[i, n] + sol[i-1, n]
+        #differenze finiti centrali
+        dsol = sol[i+1, n] - sol[i-1, n]
+        #eulero nel tempo
+        sol[i, n+1] = sol[i, n] - sol[i, n]*dt/(2*dx)*dsol + nu*dt/dx**2*ddsol
+
+    #condizioni periodiche al bordo
+    sol[0, n+1] = sol[N-1, n+1]
+    sol[N-1, n+1] = sol[1, n+1]
+
+plt.figure(1)
+freq = np.fft.rfftfreq(N, dx/(2*np.pi))
+sp_i = np.fft.rfft(sol[:, 0])
+sp_f = np.fft.rfft(sol[:,-1])
+
+plt.subplot(211)
+plt.title(f'spettro soluzione a t = 0')
+plt.ylabel("$|sol(k)|^2$", fontsize=10)
+plt.plot(freq, abs(sp_i)**2, marker='.', linestyle='')
+plt.ylim(1e-5, 1e5)
+plt.yscale('log')
+plt.grid()
+plt.subplot(212)
+plt.title(f'spettro soluzione a t = {T}')
+plt.xlabel("k", fontsize=10)
+plt.ylabel("$|sol(k)|^2$", fontsize=10)
+plt.plot(freq, abs(sp_f)**2, marker='.', linestyle='')
+plt.yscale('log')
+plt.grid()
+
+fig = plt.figure(2)
+plt.xlim(np.min(x), np.max(x))
+plt.ylim(np.min(sol), np.max(sol))
+
+line, = plt.plot([], [], 'b')
+def animate(i):
+    line.set_data(x, sol[:,i])
+    return line,
+
+
+anim = animation.FuncAnimation(fig, animate, frames=T, interval=5, blit=True, repeat=True)
+
+plt.grid()
+plt.title('burger equation')
+plt.xlabel('Distanza')
+plt.ylabel('ampiezza')
+
+#anim.save('calore.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+
+plt.show()
