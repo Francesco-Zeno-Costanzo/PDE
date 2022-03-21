49aaa19010ac8d1e47cca55807080019a93c0b50
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2022-03-21 22:08:45 +0100

    Create Laplace2D.py

---
+++ Laplace2D.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#numero punti della griglia, la spaziatura su x e y è la stessa
+N = 50
+
+x = np.linspace(0, 1, N)
+
+#Matrice creata con l'ordinamento lessicografico
+M = N**2
+A = np.zeros((M, M))
+
+for i in range(M):
+    A[i, i] = -4
+
+for i in range(M-1) :
+    A[i, i+1] = 1
+    A[i+1, i] = 1
+
+for i in range(1, N):
+    A[i*N-1, i*N] = 0
+    A[i*N, i*N-1] = 0
+
+for i in range(M-N):
+    A[i, i+N] = 1
+    A[i+N, i] = 1
+
+#termine noto
+B = np.zeros((N,N))
+
+#funzione sorgente
+y = np.zeros(N)
+y[N//4]= 1
+y[3*N//4] = -1
+B[:,:] = y#np.sin(2*np.pi*x)
+B[:,:] = B[:,:]*np.transpose(B[:,:])
+
+#dondizioni al bordo
+B[0,:] = 0#np.sin(2*np.pi*x)
+B[:,0] = 0#np.sin(2*np.pi*x)
+B[:,-1] = 0#np.sin(2*np.pi*x)
+B[-1,:] = 0#np.sin(2*np.pi*x)
+
+#faccio diventare la matrice in un vettore
+b = np.reshape(B, M)
+#risolvo
+u = np.linalg.solve(A, b)
+#faccio diventare il vettore della soluzione una matrice
+U = np.reshape(u, (N, N))
+
+#Plot
+fig = plt.figure(1)
+gridx, gridy = np.meshgrid(x, x)
+ax = fig.add_subplot(1,2,1, projection='3d')
+#plot potenziale generato dalla distribuzione
+ax.plot_surface(gridx, gridy, U, color='orange')
+ax.set_title('Soluzione equazione di laplace')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('potenziale')
+ax1 = fig.add_subplot(1,2,2, projection='3d')
+
+#plot distribuzione di carica
+ax1.plot_surface(gridx, gridy, B)
+ax1.set_title('condizioni al contorno e sorgente')
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+ax1.set_zlabel('densità')
+plt.show()
