754881a6bc55e2b6ac49e37c53efaa387a945d51
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2023-07-30 15:16:38 +0200

    small changes and adjustments

--- trasp_lax.f
+++ trasp_lax.f
@@ -1,59 +1,60 @@
-	program trasportoconlax
+      program trasplax
 C=========================================================================
-C Codice per risolvere l'equazione del trasporto: du/dt + v*du/dx = 0
-C dove v è la velocità della propagazione ed è costante con metodo di lax:
+C Code to solve the transport equation: du/dt + v*du/dx = 0. Where v is
+C the speed of the propagation and is constant. We use the lax method:
 C u^{n+1}_j = (u^n_{j+1} + u^n_{j-1})/2 - vdt/dx(u^n_{j+1} - u^n_{j-1}) 
+C The parameters are written to a file so that they are readable by both
+C the simulation code and the plot code
 c=========================================================================
 	implicit real*8 (a-h,p-z)
 	
 	real*8, dimension(:), allocatable :: sol_v, sol_n
 	
-	open(1, file='input_l.txt', status='old')	!file coi parametri
-	open(2, file='tra_lax.dat', status='unknown')	!file coi risultati
+	open(1, file='input_trasp.txt', status='old')  !parameter file
+	open(2, file='tra_lax.dat', status='unknown')  !results file
 	
-	read(1,*) N	!numeri punti sulle x
-	read(1,*) i_T	!numeri di punti nel tempo
-        read(1,*) v	!velocità di propagazione
-        read(1,*) dt	!passo temporale
-        read(1,*) dx	!passo spaziale 
-        read(1,*) i_P	!ogni quanto salvare i dati su file
+	read(1,*) N	      !nuber of ponits on spatial  axis
+	read(1,*) i_T	!nuber of ponits on temporal axis
+      read(1,*) v	      !speed of the propagation
+      read(1,*) dt	!temporal step
+      read(1,*) dx	!spatial  step 
+      read(1,*) i_P	!how often to save the solution
         
 	allocate(sol_v(0:N), sol_n(0:N))
      	
-      	alpha = v*dt/dx
-      	print*, alpha
+      alpha = v*dt/dx
+      print*, alpha
       	
-      	!condizione iniziale
-	q = 2*4*ATAN(1.) !2pigreco
-        do i = 0, N
-            x = i*dx
-            sol_v(i) = 10*sin(q*1*x) + 15*sin(q*5*x)
-      	enddo
+      !Initial condition
+	q = 2.d0 * 4.d0 * datan(1.d0) !2 pi 
+      do i = 0, N
+          x = i*dx
+          sol_v(i) = 15.d0*dsin(q*0.5d0*x)
+      enddo
       	
-      	!scrivo sul file la condizioone iniziale
-        do i = 0, N
-            write(2,*) sol_v(i)
+      !Save the initial condition
+      do i = 0, N
+          write(2,*) sol_v(i)
 	enddo
 	
-      	!evoluzione temporale con lax
+      !Time evolution
 	do i_time = 1, i_T
 	    do i_tpass = 1, i_P
 	        do i = 1, N - 1
-		    sol_n(i) = 0.5*(sol_v(i+1)*(1 - alpha))+                         
-     &		               0.5*(sol_v(i-1)*(1 + alpha))
-		enddo
+		      sol_n(i) = 0.5*(sol_v(i+1)*(1 - alpha))+                         
+     &		           0.5*(sol_v(i-1)*(1 + alpha))
+		  enddo
 		    	
-		!condizioni periodiche al bordo
-		sol_n(0) = sol_n(N - 1)
-		sol_n(N) = sol_n(1)
-		    	
-		!aggiorno la soluzione
+		  !poriodic boundary conditions
+	        sol_n(0) = sol_n(N - 1)
+		  sol_n(N) = sol_n(1)
+		  !update solutions
 	        sol_v = sol_n
 		
 	    enddo
-	    !scrivo sul file
-            do i = 0, N
-        	write(2,*) sol_v(i)
+	    !save the solution on file
+          do i = 0, N
+        	  write(2,*) sol_v(i)
 	    enddo
 	    
 	enddo
@@ -61,4 +62,4 @@ c=========================================================================
 	deallocate(sol_v, sol_n)
 	stop
 	
-	end program trasportoconlax
+	end program trasplax
