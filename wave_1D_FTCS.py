8166060f28ec483769ecbcc2f836265d399a7c6a
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2023-07-30 15:30:15 +0200

    FTCS for wave 1D

--- wave_1D_FTCS.py
+++ wave_1D_FTCS.py
@@ -1,35 +1,38 @@
+"""
+code for solving the wave equation in one spatial dimension using the FTCS method
+"""
 import numpy as np
 import matplotlib as mp
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 
 
-N = 125
-t = 315
-#c deve essere minore di uno
+N = 125  # point on spatial  axis
+t = 315  # point on temporal axis
+
+#c must be less equal than 1
 #c = v*dt**2/dx**2
 c = 1
 y = np.linspace(0, 1, N)
 U = np.zeros((N,t))
-U[0:N,0] = np.sin(2*np.pi*y)
+U[0:N,0] = np.sin(2*np.pi*y) # initial condition
 
 for j in np.arange(0,t-1):
     for i in np.arange(0,N-1):
         U[i, j+1] = 2*U[i,j]-U[i,j-1]+ c*(U[i+1, j] - 2*U[i, j] + U[i-1, j])
 
-##
-#la simulazione restituisce un'onda la cui ampiezza e maggiore della
-#condizione inizale. Probabilmente ciò è dovuto a problemi del metodo adottato
-##
+#===============================================================================
+# The simulation returns a wave whose amplitude is greater than initial
+# condition. This is probably due to problems with the method used
+#===============================================================================
 
 fig = plt.figure(1)
-ax = fig.gca(projection='3d')
+ax = fig.add_subplot(projection='3d')
 
-ax.set_title('Propagazione onde')
-ax.set_xlabel('Tempo')
-ax.set_ylabel('Lunghezza')
-ax.set_zlabel('Ampiezza')
+ax.set_title('Wave propagation')
+ax.set_xlabel('Time')
+ax.set_ylabel('Distance')
+ax.set_zlabel('Amplitudde')
 
 
 gridx, gridy = np.meshgrid(range(t), y)
@@ -48,9 +51,9 @@ def animate(i):
 anim = animation.FuncAnimation(fig, animate, frames=t ,interval=20, blit=True, repeat=True)
 
 plt.grid()
-plt.title('Propagazione onde')
-plt.xlabel('Distanza')
-plt.ylabel('Ampiezza')
+plt.title('Wave propagation')
+plt.xlabel('Distance')
+plt.ylabel('Amplitudde')
 
 #anim.save('onda.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
